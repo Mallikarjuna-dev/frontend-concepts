ad5ae1c6e258bad9c154ac3d166eb27f7e14ccc9
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2025-06-02 23:41:29 +0530

    updates

--- introToFetch/displayUsers.js
+++ introToFetch/displayUsers.js
@@ -1,14 +1,15 @@
 
 function fetchData() {
+
     fetch('https://jsonplaceholder.typicode.com/users')
         .then((res) => res.json())
         .then((user) => {
             user.forEach((user) => {
-                console.log(user.name)
+                console.log(user.name);
             })
         })
         .catch((error) => {
-            console.error('Error fetching users:', error)
+            console.error('Error fetching users:', error);
         })
 }
 
