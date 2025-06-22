034e092abe00c2e67b12d6c4fc27f0a60e6e26f2
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2025-06-22 18:21:23 +0530

    intro redux toolkit

---
+++ redux-toolkit/src/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, reset } from './counterSlice';
+
+const Counter = () => {
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
+        </div>
+
+    )
+}
+
+export default Counter
